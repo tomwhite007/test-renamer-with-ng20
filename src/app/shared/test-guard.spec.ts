4e896db6d8340ab9a390bef701c6ef5c6504ffe9
Tom White <tomwhite007@gmail.com>
2025-06-17 21:19:30 +0100

    fix: test import after file move

--- src/app/shared/test-guard.spec.ts
+++ src/app/shared/test-guard.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { testGuard } from '../test-guard';
+import { testGuard } from './test-guard';
 
 describe('testGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
