d72043ce94ccfc00eed826c64c942e8ff1ae4a9d
Tom White <tomwhite007@gmail.com>
2025-06-18 20:28:37 +0100

    feat: add resolver and interceptor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { testGuard } from './shared/test-guard';
+import { testResolver } from './shared/test-resolver';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent, canActivate: [testGuard] },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [testGuard],
+    resolve: testResolver,
+  },
   { path: '**', redirectTo: '/home' },
 ];
