f93646147d1dbc7203726aaf885cc6f6a3069b56
Tom White <tomwhite007@gmail.com>
2025-06-20 21:25:49 +0100

    feat: add dependency on TestUnrecognisedDefinition

--- src/app/shared/test-guard.ts
+++ src/app/shared/test-guard.ts
@@ -1,5 +1,8 @@
 import { CanActivateFn } from '@angular/router';
+import { TestUnrecognisedDefinition } from './test-unrecognised-definition';
 
 export const testGuard: CanActivateFn = (route, state) => {
+  const unrecognisedDefinition: TestUnrecognisedDefinition =
+    'unrecognised-definition';
   return true;
 };
