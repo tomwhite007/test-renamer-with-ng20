1679f1a31e781ae946705cfcd5d72b1997405acc
Tom White <tomwhite007@gmail.com>
2025-06-20 21:06:02 +0100

    feat: add unrecognised definition

---
+++ src/app/shared/test-unrecognised-definition.ts
@@ -0,0 +1,11 @@
+export type TestUnrecognisedDefinition =
+  | 'unrecognised-definition'
+  | 'unrecognised-definition-2'
+  | 'unrecognised-definition-3'
+  | 'unrecognised-definition-4'
+  | 'unrecognised-definition-5'
+  | 'unrecognised-definition-6'
+  | 'unrecognised-definition-7'
+  | 'unrecognised-definition-8'
+  | 'unrecognised-definition-9'
+  | 'unrecognised-definition-10';
